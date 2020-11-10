664af7b63ff01c8d9621ca21a4ae4688def43a62
ilaksono <ian.laksono@gmail.com>
2020-11-10 21:31:53 +0000

    add about view

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  resources :about do
+    root to: "about#show"
+  end
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
