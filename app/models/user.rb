9880f57820ebe64a1032266d483843b9d0839b57
ilaksono <ian.laksono@gmail.com>
2020-11-16 16:02:34 -0500

    add user class method auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  has_secure_password  
+  has_secure_password
+  def self.authenticate_with_credentials email, password
+    if User.find_by email: email
+      return nil
+    elsif password.length < 5
+      return nil
+    else
+      return User.new(email: email, password: password)
+  end  
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "password" do
+    it "must be created with pass and confirmation fields" do
+    end
+
+    it "should have password min length when acc created" do
+    end
+  
+  end
+  context "email" do 
+    it "must be unique" do
+    
+    end
+    it "should not be case sensitive" do
+    
+    end
+
+    it "should have email, first name, last name required" do
+    
+    end
+  end
+end
