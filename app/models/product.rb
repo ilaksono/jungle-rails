49297421a5c4cef535a9dde859ddeaedb3e8b287
ilaksono <ian.laksono@gmail.com>
2020-11-16 19:59:14 +0000

    add rspec testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,12 @@ class Product < ActiveRecord::Base
   validates :quantity, presence: true
   validates :category, presence: true
 
+  def sold_out?
+    if quantity < 1
+      return true
+    else
+      return false
+    end
+  end
+
 end
